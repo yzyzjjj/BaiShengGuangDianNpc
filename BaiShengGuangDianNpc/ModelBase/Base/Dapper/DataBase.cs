71955ac805873c3932c25e5085287b6c8566682c
yzyzjjj <1042773917@qq.com>
2019-06-22 14:06:13 +0800

    database

--- BaiShengGuangDianNpc/ModelBase/Base/Dapper/DataBase.cs
+++ BaiShengGuangDianNpc/ModelBase/Base/Dapper/DataBase.cs
@@ -20,6 +20,18 @@ namespace ModelBase.Base.Dapper
                 return con.Query<T>(sql, param);
             }
         }
+        public IEnumerable<T> QueryWithTime<T>(string sql, object param = null, int? second = null)
+        {
+            if (second == null)
+            {
+                second = 60;
+            }
+
+            using (var con = new MySqlConnection(_connectionString))
+            {
+                return con.Query<T>(sql, param, null, true, second);
+            }
+        }
 
         public int Execute(string sql, object param = null)
         {
